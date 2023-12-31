7359c3158f7e3c1db7db2e738222f765413d03ac
Walid Saifi <saifiwalid21@gmail.com>
2023-12-31 21:19:09 +0100

    Update get_data.php

--- get_data.php
+++ get_data.php
@@ -7,9 +7,6 @@ $db   = 'per';
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-if ($conn->connect_error) {
-    die("La connexion à la base de données a échoué: " . $conn->connect_error);
-}
 
 // Exécutez votre requête SQL pour récupérer les données (remplacez le SELECT par votre propre requête)
 $query = "SELECT *  FROM utilisateurs";
@@ -20,7 +17,30 @@ $data = array();
 while ($row = $result->fetch_assoc()) {
     $data[] = $row;
 }
-
 // Renvoyer les données au format JSON
 echo json_encode($data);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    $nom = $_POST['nom'];
+    $email = $_POST['email'];
+    if (!empty($nom) && !empty($email)) {
+        // Utiliser une requête préparée pour éviter les attaques par injection SQL
+        $sql = "INSERT INTO utilisateurs (nom, email, prenom, age) VALUES (?, ?, null, null)";
+        
+        // Préparer la requête
+        $stmt = $conn->prepare($sql);
+
+        // Binder les paramètres
+        $stmt->bind_param('ss', $nom, $email);
+
+        // Exécuter la requête
+        $res = $stmt->execute();
+    }
+    
+
+}
+
+
+
+
 ?>
